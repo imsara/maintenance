67f0ba5b682ddb89cf78736658bd02ce67edd438
TatuKalermo <tatukalermo@gmail.com>
2019-11-14 19:39:33 +0200

    Added sailiontaytto page

--- src/MyApp.js
+++ src/MyApp.js
@@ -3,24 +3,26 @@ let MyApp = (function() {
     let poiViewer = null;
 
     let initPoiViewer = function(selector) {
-        let closeButton = document.querySelector(selector + " button");
+        /*let closeButton = document.querySelector(selector + " button");
             closeButton.onclick = closePoi;
         let title = document.querySelector(selector + " #myPOITitle");
-        let description = document.querySelector(selector + " #myPOIDescription");
+        let description = document.querySelector(selector + " #myPOIDescription");*/
+        let closeButton = document.querySelector(selector + " #btnCancel");
+            closeButton.addEventListener("click", closePoi);
+        let title = document.querySelector(selector + " .modalTitle");
+        let description = document.querySelector(selector + " .modalSubTitle");
     
         poiViewer = {
-          dom: document.querySelector(selector),
-          title: title,
-          description: description,
-          closeButton: closeButton
+          dom: document.querySelector(selector)
         }
     };
 
     let poiSelected = function(poi) {
         let lang = IV.api.ui.languageService.getCurrentLanguage();  // Obtain current IndoorViewer language
-        poiViewer.title.innerHTML = poi.titles[lang];
-        poiViewer.description.innerHTML = poi.descriptions[lang];
-        poiViewer.dom.style.display = "block";  // Display HTML template
+        console.log(poiViewer);
+        //poiViewer.title.innerHTML = poi.titles[lang];
+        //poiViewer.description.innerHTML = poi.descriptions[lang];
+        poiViewer.dom.style.display = "flex";  // Display HTML template
     }
 
     let closePoi = function() {
@@ -28,9 +30,10 @@ let MyApp = (function() {
         poiService.closePOI();
         poiViewer.dom.style.display = "none";
     }
+
     return {
         initOverrides: function() {
-          initPoiViewer("#myPOIViewer");
+          initPoiViewer(".modal");
           IV.api.poi.service.onPoiClick.connect(function(poi) { poiSelected(poi); });
         }
     };
