55bfe747825c939f1d4881d5232b0278a32d0c81
tuomaju <Tuomas.Juopperi@metropolia.fi>
2019-11-24 21:06:03 +0200

    simppeli router

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,43 +1,79 @@
 let MyApp = (function() {
 
     let poiViewer = null;
+    let routerItems = document.querySelectorAll(".routerContainer .routerItem");
+    console.log(routerItems);
 
-    let initPoiViewer = function(selector) {
+    /* elements for säiliöntäyttö */
+    let sailioCloseButton = document.querySelector(".routerSailio #btnCancel");
+    let title = document.querySelector("routerSailio .modalTitle");
+    let description = document.querySelector("routerSailio .modalSubTitle");
+    let sailioSucceedButton = document.querySelector("#btnSucceed");
+    sailioSucceedButton.addEventListener("click" , () => {
+        router(0);
+    });
+    sailioCloseButton.addEventListener("click", () => {
+        router(0);
+    });
+
+
+    /* elements for pumpoptions */
+    let pumpSailioButton = document.querySelector(".taytto button");
+    pumpSailioButton.addEventListener("click" , () => {
+        router(1);
+    });
+    let pumpCloseButton = document.querySelector(".v");
+
+
+
+    const initPoiViewer = function(selector) {
         /*let closeButton = document.querySelector(selector + " button");
             closeButton.onclick = closePoi;
         let title = document.querySelector(selector + " #myPOITitle");
         let description = document.querySelector(selector + " #myPOIDescription");*/
-        let closeButton = document.querySelector(selector + " #btnCancel");
-            closeButton.addEventListener("click", closePoi);
-        let title = document.querySelector(selector + " .modalTitle");
-        let description = document.querySelector(selector + " .modalSubTitle");
-    
+        pumpCloseButton.addEventListener("click", closePoi);
         poiViewer = {
           dom: document.querySelector(selector)
         }
     };
 
-    let poiSelected = function(poi) {
+    const poiSelected = function(poi) {
         let lang = IV.api.ui.languageService.getCurrentLanguage();  // Obtain current IndoorViewer language
         console.log(poiViewer);
         //poiViewer.title.innerHTML = poi.titles[lang];
         //poiViewer.description.innerHTML = poi.descriptions[lang];
-        document.querySelector(".modalContainer").style.display = "flex";
-        document.querySelector(".modalContainer .backDrop").style.display = "flex";
+        document.querySelector(".routerContainer").style.display = "flex";
+        document.querySelector(".routerContainer .backDrop").style.display = "flex";
         poiViewer.dom.style.display = "flex";  // Display HTML template
     }
 
-    let closePoi = function() {
+    const closePoi = function() {
         let poiService = IV.api.poi.service;
         poiService.closePoi();
-        document.querySelector(".modalContainer").style.display = "none";
-        document.querySelector(".modalContainer .backDrop").style.display = "none";
+        document.querySelector(".routerContainer").style.display = "none";
+        document.querySelector(".routerContainer .backDrop").style.display = "none";
+        for (routerItem of routerItems){
+            routerItem.style.display = "none";
+        }
         poiViewer.dom.style.display = "none";
+    }
+
+
+    /* shows the element given as parameter, hides other elements */
+    const router = function(n) {
+        for (routerItem of routerItems){
+            if (routerItem !== routerItems[n]) {
+                routerItem.style.display = "none";
+                console.log(routerItem)
+            } else {
+                routerItem.style.display = "flex";
+            }
+        }
     }
 
     return {
         initOverrides: function() {
-          initPoiViewer(".modal");
+          initPoiViewer(".routerPump");
           IV.api.poi.service.onPoiClick.connect(function(poi) { poiSelected(poi); });
         }
     };
