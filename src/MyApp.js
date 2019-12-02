a9104cb9e295719ba4a5ba50e6f8404f5f49188d
TatuKalermo <tatukalermo@gmail.com>
2019-12-02 04:31:46 +0200

    added pi-kaavio

--- src/MyApp.js
+++ src/MyApp.js
@@ -6,18 +6,24 @@ let MyApp = (function() {
 
     /* elements for säiliöntäyttö */
     let sailioCloseButton = document.querySelector(".routerSailio #btnCancel");
-    let title = document.querySelector("routerSailio .modalTitle");
-    let description = document.querySelector("routerSailio .modalSubTitle");
     let sailioSucceedButton = document.querySelector("#btnSucceed");
+    let kaavioCloseButton = document.querySelector(".routerKaavio #btnClose")
     let video = document.querySelector("#video");
+    let kaavioButton = document.querySelector("#btnKaavio");
     sailioSucceedButton.addEventListener("click" , () => {
         router(0);
     });
     sailioCloseButton.addEventListener("click", () => {
         router(0);
     });
+    kaavioCloseButton.addEventListener("click", () => {
+        router(0);
+    });
     video.addEventListener("click", () => {
-        link();
+        link("https://esite.viar360.com/virtual/story/play/F9A6498E-181D-0B14-A285-E74CF38E954A?getiframedimensions=true&ui=false&video_cmd=false");
+    });
+    kaavioButton.addEventListener("click", () => {
+        router(2);
     });
 
 
@@ -31,10 +37,6 @@ let MyApp = (function() {
 
 
     const initPoiViewer = function(selector) {
-        /*let closeButton = document.querySelector(selector + " button");
-            closeButton.onclick = closePoi;
-        let title = document.querySelector(selector + " #myPOITitle");
-        let description = document.querySelector(selector + " #myPOIDescription");*/
         pumpCloseButton.addEventListener("click", closePoi);
         poiViewer = {
           dom: document.querySelector(selector)
@@ -44,8 +46,6 @@ let MyApp = (function() {
     const poiSelected = function(poi) {
         let lang = IV.api.ui.languageService.getCurrentLanguage();  // Obtain current IndoorViewer language
         console.log(poiViewer);
-        //poiViewer.title.innerHTML = poi.titles[lang];
-        //poiViewer.description.innerHTML = poi.descriptions[lang];
         document.querySelector(".routerContainer").style.display = "flex";
         document.querySelector(".routerContainer .backDrop").style.display = "flex";
         poiViewer.dom.style.display = "flex";  // Display HTML template
@@ -75,9 +75,9 @@ let MyApp = (function() {
         }
     }
 
-    /* Opens a link to the 360 video*/
-    const link = function(){
-        window.open("https://esite.viar360.com/virtual/story/play/F9A6498E-181D-0B14-A285-E74CF38E954A?getiframedimensions=true&ui=false&video_cmd=false", "_blank");
+    /* Opens a specified link in a new tab*/
+    const link = function(link){
+        window.open(link, "_blank");
     }
 
     return {
